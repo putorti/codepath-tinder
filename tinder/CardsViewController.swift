98108f394b4e4f7be9dfb2ec6d1b8dac9631a0df
Jason Putorti <jason@brigade.com>
2016-03-02 19:22:21 -0800

    Initial checkin

---
+++ tinder/CardsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardsViewController.swift
+//  tinder
+//
+//  Created by Jason Putorti on 3/2/16.
+//  Copyright © 2016 Jason Putorti. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
